c4a759f2d992e183e0df7fafba0a74f13ae5f93c
Daniel Carvalho <daniel.gdc22@gmail.com>
2024-07-23 09:03:50 -0300

    feat: tailwind and typescript installation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='text-2xl text-red-200'>So Far So God</h2>
+    </>
+  )
+}
+
+export default App
